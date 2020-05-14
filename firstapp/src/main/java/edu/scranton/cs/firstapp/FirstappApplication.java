72080e7bb486f3824767b5f777c86c11c3ca4b8b
Andy Buttner <andrew.buttner@scranton.edu>
2020-05-14 09:04:19 -0400

    Initial commit

---
+++ firstapp/src/main/java/edu/scranton/cs/firstapp/FirstappApplication.java
@@ -0,0 +1,13 @@
+package edu.scranton.cs.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstappApplication.class, args);
+	}
+
+}
